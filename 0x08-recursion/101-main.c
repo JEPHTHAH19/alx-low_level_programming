b0cf93319651e3c7e99f302705ea47f48e8c9f42
JEPHTHAH19 <ampah304@gmail.com>
2023-03-12 14:01:25 -0700

    task1

--- 0x08-recursion/101-main.c
+++ 0x08-recursion/101-main.c
@@ -10,8 +10,8 @@ int main(void)
 {
 	int r;
 
-	r = wildcmp("main.c", "*.c");
-	printf("%d\n", r);
+
+	
 	r = wildcmp("main.c", "m*a*i*n*.*c*");
 	printf("%d\n", r);
 	r = wildcmp("main.c", "main.c");
